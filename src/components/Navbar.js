52f93035ea6d664b877ad4d43f90ce71648b1061
Adin Solomon <adinbsolomon@gmail.com>
2022-03-22 15:09:46 -0400

    correct navbar to projects

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -68,7 +68,7 @@ function NavBar() {
             <Nav.Item>
               <Nav.Link
                 as={Link}
-                to="/project"
+                to="/projects"
                 onClick={() => updateExpanded(false)}
               >
                 <AiOutlineFundProjectionScreen
