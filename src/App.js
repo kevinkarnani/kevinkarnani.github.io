324213bd14dbca9235aab7701c0c6732798fae19
Adin Solomon <adinbsolomon@gmail.com>
2022-03-22 15:00:32 -0400

    correct projects url

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
         <Navbar />
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/project" component={Projects} />
+          <Route path="/projects" component={Projects} />
           <Route path="/about" component={About} />
           <Route path="/resume" component={Resume} />
         </Switch>
