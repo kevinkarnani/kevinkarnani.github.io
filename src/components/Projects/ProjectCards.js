e53c5ebe28a4e7e854f3d2b4e11376ca394fe785
Adin Solomon <adinbsolomon@gmail.com>
2022-03-22 14:59:00 -0400

    parameterize project card

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -1,20 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { BiLinkExternal } from "react-icons/bi";
 
 function ProjectCards(props) {
+
+  const [img, setImg] = useState("");
+
+  const load_img = (imgPath) => {
+    import(`../../Assets/Projects/${imgPath}`).then(
+      (img) => {setImg(img.default)}
+    );
+  }
+  load_img(props.imgPath);
+
   return (
     <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+      <Card.Img variant="top" src={img} alt="card-img" />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
         <Button variant="primary" href={props.link} target="_blank">
-          <BiLinkExternal /> &nbsp;
-          {props.isBlog ? "View Blog" : "View Project"}
+          <BiLinkExternal /> &nbsp;View Project
         </Button>
       </Card.Body>
     </Card>
