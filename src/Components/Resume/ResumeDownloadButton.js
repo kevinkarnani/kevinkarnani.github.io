bbb07176b113bfe87db0810b28b7bd2c0ff943e8
Ritik Ghanshani <ghanshaniritik@gmail.com>
2022-03-29 23:50:56 -0400

    fixed resume download link

--- src/Components/Resume/ResumeDownloadButton.js
+++ src/Components/Resume/ResumeDownloadButton.js
@@ -3,9 +3,14 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import { AiOutlineDownload } from "react-icons/ai";
 
-import ResumeFile from "../../Assets/Data/resume.pdf";
+// import ResumeFile from "../../Assets/Data/resume.pdf";
+
+
 
 function ResumeDownloadButton() {
+
+  let ResumeFile = "https://raw.githubusercontent.com/ritik-ghanshani/ritik-ghanshani-resume/main/Ritik_Ghanshani_Resume.pdf";
+
   return <Button variant="primary" href={ResumeFile} target="_blank">
     <AiOutlineDownload />
     &nbsp;Download Resume
